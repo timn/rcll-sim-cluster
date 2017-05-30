b0335581e42f8ec1392ec9295a69b7ff4efd37a4
Tim Niemueller <niemueller@kbsg.rwth-aachen.de>
2017-05-30 10:58:20 +0200

    kubernetes/ceph: no longer wait for ceph-mon
    With external DNS no longer required.

--- kubernetes/ceph/create_rcll.sh
+++ kubernetes/ceph/create_rcll.sh
@@ -11,27 +11,27 @@
 kubectl create -f rbd-storage-class.yaml
 
 #kubectl create -f rbd-storage-class.yaml
-echo "Waiting for monitoring pod to come up"
-while true; do
-  POD_NAME=$(kubectl --namespace=ceph get pods -o custom-columns=":.metadata.name" 2>/dev/null | grep ceph-mon | grep -v ceph-mon-check)
-	if [ -n "$POD_NAME" ]; then break; fi
-	sleep 1
-done
+#echo "Waiting for monitoring pod to come up"
+#while true; do
+#  POD_NAME=$(kubectl --namespace=ceph get pods -o custom-columns=":.metadata.name" 2>/dev/null | grep ceph-mon | grep -v ceph-mon-check)
+#	if [ -n "$POD_NAME" ]; then break; fi
+#	sleep 1
+#done
 
-echo "Monitoring Pod: $POD_NAME"
+#echo "Monitoring Pod: $POD_NAME"
 
-while true; do
-	POD_IP=$(kubectl --namespace=ceph get pods $POD_NAME -o custom-columns=":.metadata.name,:.status.podIP" 2>/dev/null | grep ceph-mon | awk '{ print $2 }')
-	if [ -n "$POD_IP" ]; then break; fi
-	sleep 1
-done
+#while true; do
+#	POD_IP=$(kubectl --namespace=ceph get pods $POD_NAME -o custom-columns=":.metadata.name,:.status.podIP" 2>/dev/null | grep ceph-mon | awk '{ print $2 }')
+#	if [ -n "$POD_IP" ]; then break; fi
+#	sleep 1
+#done
 
-echo "Monitoring Pod IP: $POD_IP"
+#echo "Monitoring Pod IP: $POD_IP"
 
-cat >/tmp/pod_ip <<EOM
-monitor_ip: !!str $POD_IP
-EOM
-jinja2 --format=yaml generator/templates/ceph/rbd-storage-class.yaml.jinja /tmp/pod_ip >/tmp/rbd.yaml
-kubectl create -f /tmp/rbd.yaml
-rm /tmp/pod_ip /tmp/rbd.yaml
+#cat >/tmp/pod_ip <<EOM
+#monitor_ip: !!str $POD_IP
+#EOM
+#jinja2 --format=yaml generator/templates/ceph/rbd-storage-class.yaml.jinja /tmp/pod_ip >/tmp/rbd.yaml
+#kubectl create -f /tmp/rbd.yaml
+#rm /tmp/pod_ip /tmp/rbd.yaml
 
