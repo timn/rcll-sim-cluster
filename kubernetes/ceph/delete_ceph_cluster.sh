22c633e4bc21698f9ca7b477710cc8d2985ac871
Tim Niemueller <niemueller@kbsg.rwth-aachen.de>
2018-06-14 15:32:04 +0200

    k8s/ceph: update scripts
    Some things renamed or added.

--- kubernetes/ceph/delete_ceph_cluster.sh
+++ kubernetes/ceph/delete_ceph_cluster.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 kubectl delete namespace ceph
-kubectl delete secret ceph-admin-secret --namespace=kube-system
+kubectl delete secret ceph-secret-admin --namespace=kube-system
 kubectl delete secret ceph-client-key --namespace=kube-system
 kubectl delete secret ceph-client-key --namespace=default
 kubectl delete secret ceph-client-key --namespace=database
